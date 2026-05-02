049eb6ee7d47dfd3329bde856d3407ba6f66d5ec
UserTics <ticsconproposito@gmail.com>
2026-05-02 10:44:02 -0600

    log para fecha

--- src/main/java/BackERP/controller/EncabezadoFacturasRestController.java
+++ src/main/java/BackERP/controller/EncabezadoFacturasRestController.java
@@ -90,7 +90,7 @@ public class EncabezadoFacturasRestController {
     public ResponseEntity<Long> grabarEncabezadoFacturas(@RequestBody erpEncabezadoFacturas EncabezadoFacturas){
 
         // valor por defecto
-
+System.out.println("Fecha "+ EncabezadoFacturas.getFechaFactura());
         EncabezadoFacturas.setFechaModificacion(LocalDate.now());
         EncabezadoFacturas.setHoraModificacion(LocalTime.now());
         EncabezadoFacturas.setReversion("N");
