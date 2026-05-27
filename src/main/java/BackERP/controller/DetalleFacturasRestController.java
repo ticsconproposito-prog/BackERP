e2137b2f785cbcab4c267055d50bde1946b8ecee
Merlyn Mariolis Franco Arredondo <mfranco@plusti.com>
2026-05-27 12:10:01 -0600

    se corrige ordenamiento de consulta

--- src/main/java/BackERP/controller/DetalleFacturasRestController.java
+++ src/main/java/BackERP/controller/DetalleFacturasRestController.java
@@ -31,7 +31,7 @@ public class DetalleFacturasRestController {
     @RequestParam(required = false) Integer idEncabezadoFactura,
     @RequestParam(required = false) LocalDate fechaInicio,
     @RequestParam(required = false) LocalDate fechaFin,
-    @RequestParam(defaultValue = "idEncabezadoFactura,asc") String sort) {
+    @RequestParam(defaultValue = "idDetalleFactura,asc") String sort) {
 
     Specification<erpDetalleFacturas> spec = Specification
       .where(erpDetalleFacturaSpecs.idEncabezadoFacturaContains(idEncabezadoFactura))
