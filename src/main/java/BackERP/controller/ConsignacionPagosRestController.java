b07b348b63def3af1eefaf881b19d57149d3323f
UserTics <ticsconproposito@gmail.com>
2026-07-23 22:05:42 -0600

    Se modifica la url para el consumo de api ConsignacionPagosRestController

--- src/main/java/BackERP/controller/ConsignacionPagosRestController.java
+++ src/main/java/BackERP/controller/ConsignacionPagosRestController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 //@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/consignacionPagos")
+@RequestMapping("/consignacionPagos")
 public class ConsignacionPagosRestController {
 
   @Autowired
