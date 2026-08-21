538271f718db8fd98871482cb7072fbbd27f4092
UserTics <ticsconproposito@gmail.com>
2026-08-21 00:39:35 -0600

    Se modifica url de InventarioReordenamientoController.java se quita /api

--- src/main/java/BackERP/controller/InventarioReordenamientoController.java
+++ src/main/java/BackERP/controller/InventarioReordenamientoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/inventario")
+@RequestMapping("/inventario")
 public class InventarioReordenamientoController {
 
   @Autowired
